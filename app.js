b31928e9822dcac11b307beb82153da780178e83
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2021-12-06 18:14:23 -0500

    stylesheet linked and code refactored

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
@@ -85,7 +85,7 @@ Add a New Project
         },
       },
       {
-        type: "checkboxes",
+        type: "checkbox",
         name: "languages",
         message: "What did you build this project with? (Check all that apply)",
         choices: [
@@ -137,11 +137,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw err;
-
-      console.log("page Created! Check out index.html to see the output!");
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
